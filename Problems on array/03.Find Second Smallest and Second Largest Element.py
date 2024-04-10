bee42d045c3352c9285328406c20be8f735ceaeb
Poorani <tpoorani2002@gmail.com>
2024-04-10 22:54:10 +0530

    03

--- Problems on array/03.Find Second Smallest and Second Largest Element.py
+++ Problems on array/03.Find Second Smallest and Second Largest Element.py
@@ -21,6 +21,7 @@ Explanation:
 
 def solution_1 (arr):
     arr=set(sorted(arr))
+    print(arr)
     arr=list(arr)
     if len(arr) >2 :
         print(f"second smallest element {arr[1]}\n second largest element {arr[-2]}")
@@ -29,8 +30,32 @@ def solution_1 (arr):
     else:
         print("-1")
 
+def solution_2(arr, n):
+    if n == 0 or n == 1:
+        print(-1, -1)  # edge case when only one element is present in array
+    small = float('inf')
+    second_small = float('inf')
+    large = float('-inf')
+    second_large = float('-inf')
+    for i in range(n):
+        small = min(small, arr[i])
+        large = max(large, arr[i])
+    for i in range(n):
+        if arr[i] < second_small and arr[i] != small:
+            second_small = arr[i]
+        if arr[i] > second_large and arr[i] != large:
+            second_large = arr[i]
+    print("Second smallest is", second_small)
+    print("Second largest is", second_large)
+
+     
+
+
+
 arr= list(map(int,input().split()))
+n=len(arr)
 solution_1(arr)
+solution_2(arr,n)
 
 
 
