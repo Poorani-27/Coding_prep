39e3d652b0b759fb0a4829aadd2766b81b06041e
Poorani <tpoorani2002@gmail.com>
2024-04-15 18:56:55 +0530

    06

--- Problems on array/10.find median of the array.py
+++ Problems on array/10.find median of the array.py
@@ -18,7 +18,6 @@ What is a Median?
 Median is defined as the value which is present in the middle for a series of values. Note, in order to find the median of an array of integers, we must make sure they are sorted.
 
 '''
-
 arr = [2,5,1,7]
 arr.sort()
 n= len(arr)
@@ -28,3 +27,17 @@ else :
     median = arr[n//2]
 print(median)
 
+'''
+
+Time Complexity:
+
+Sorting the array arr using the sort() method takes O(n log n) time, where n is the number of elements in the array. Sorting dominates the time complexity.
+Finding the median involves constant-time operations such as accessing elements by index and performing arithmetic operations.
+Therefore, the overall time complexity is O(n log n).
+Space Complexity:
+
+The space complexity is O(n) due to the space required to store the array arr.
+Other variables like n and median require constant space, which does not depend on the size of the input array.
+Therefore, the overall space complexity is O(n).
+
+'''
