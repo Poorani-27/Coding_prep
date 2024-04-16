b545bcbdf56051353588b9de0942414e7b1dc113
Poorani <tpoorani2002@gmail.com>
2024-04-16 22:44:52 +0530

    08

---
+++ Problems on array/18.finding equilibrium index.py
@@ -0,0 +1,47 @@
+'''
+
+Problem Statement: Finding Equilibrium index in an array
+
+Given a 0-indexed integer array nums, find the leftmost equilibrium Index.
+
+An equilibrium Index is an index at which sum of elements on its left is equal to the sum of element on its right. That is, nums[0] + nums[1] + ... + nums[equilibriumIndex-1] == nums[equilibriumIndex+1] + nums[equilibriumIndex+2] + ... + nums[nums.length-1]. If equilibriumIndex == 0, the left side sum is considered to be 0. Similarly, if equilibriumIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost equilibrium Index that satisfies the condition, or -1 if there is no such index.
+
+Examples:
+
+Example 1:
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
+
+Example 2:
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
+
+
+
+'''
+
+
+
+
+
+def equilibrium_index(arr):
+    n = len(arr)
+    total_sum=sum(arr)
+    left_sum =0
+    for i in range(n):
+        total_sum=total_sum - arr[i]
+        if left_sum ==total_sum :
+            return i
+        left_sum=left_sum+arr[i]
+    return -1
+
+
+
+arr=[1,-1,4]
+print(equilibrium_index(arr))
