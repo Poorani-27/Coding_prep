1cdd3666d735def0ce25039347744540c5e8e404
Poorani <tpoorani2002@gmail.com>
2024-04-16 21:16:42 +0530

    08

--- Problems on array/14.finding all the repeating elements in the array.py
+++ Problems on array/14.finding all the repeating elements in the array.py
@@ -1,24 +1,18 @@
-def finding_repeating(arr):
-    frequency = {}
-    repeating_elements = []
-
-    # Count the frequency of each element
-    for num in arr:
+def finding_repeating(arr,n):
+    frequency ={}
+    repeating_elemnts =[]
+    for num in arr :
         if num in frequency:
-            frequency[num] += 1
-        else:
+            frequency[num] +=1
+
+        else :
             frequency[num] = 1
+    for key,value in frequency.items() :
+        if value >1 :
+            repeating_elemnts.append(key)
+    print(repeating_elemnts)
 
-    # Find elements with frequency greater than 1
-    for key, value in frequency.items():
-        if value > 1:
-            repeating_elements.append(key)
 
-    return repeating_elements
-
-# Test Examples
-arr1 = [1, 1, 2, 3, 4, 4, 5, 2]
-arr2 = [1, 1, 0]
-
-print(finding_repeating(arr1))  # Output: [1, 2, 4]
-print(finding_repeating(arr2))  # Output: [1]
+arr=[1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,5]
+n=len(arr)
+finding_repeating(arr,n)
