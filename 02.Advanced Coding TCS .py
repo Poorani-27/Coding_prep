0d09f9c89d7e1749f2bc41729ef3782b6c9dd4ca
Poorani <tpoorani2002@gmail.com>
2024-04-18 21:35:34 +0530

    10

--- 02.Advanced Coding TCS .py
+++ 02.Advanced Coding TCS .py
@@ -4,7 +4,7 @@
  Test cases 
  input 4567 
  expected outcome 2
-   explanation : odd positions are four and 6 as the part of their position 1 and position 3 Both have sum 10. 
+   explanation : odd positions are 4 and 6 as the part of their position 1 and position 3 Both have sum 10. 
    Similarly, 5 and seven are at the position of 2 and 4 with the sum 12. 
    The difference between 12 - 10 equal to two.
 
@@ -14,8 +14,14 @@
 def odd_even(num):
     odd=[]
     even=[]
-    pass
+    for index, i in enumerate(num):
+        if index %2==0 :
+            even.append(index)
+        else :
+            odd.append(index)
+    print(abs(sum(odd)-sum(even)))
+
 
 num=4567
-num=list(num)
-print(num(1))
+num=str(num)
+odd_even(num)
